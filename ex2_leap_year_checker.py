55c6f72a8d4e038476371b97a02cc026be7a04b2
Ben-Blueinno <139459995+Ben-Blueinno@users.noreply.github.com>
2024-09-21 11:54:55 +0800

    Update ex2_leap_year_checker.py

--- ex2_leap_year_checker.py
+++ ex2_leap_year_checker.py
@@ -4,11 +4,4 @@
 # >>This is a leap year.
 # >>Input a year: 2100
 # >>This is not a leap year.
-year = int(input("input a year: "))
 
-if year % 400 == 0:
-    print("This is a leap year.")
-elif year % 4 == 0 and not(year % 100 == 0):
-    print("This is a leap year.")
-else:
-    print("This is not a leap year.")
